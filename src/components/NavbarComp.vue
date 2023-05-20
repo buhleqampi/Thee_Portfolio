ce94c4478cc7e7716ae40e78bdbc786552c0ae84
buhleqampi <buhle3857@gmail.com>
2023-05-20 12:57:42 +0200

    mom

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -1,29 +1,38 @@
 <template>
-<nav class="navbar navbar-expand-md"> 
-<ul>
-<li style="display:inline" class="logo">
-<img src="https://i.postimg.cc/HkZM7fBz/logo2.png" alt="logo"/>
-</li>
-<li style="display:inline" id="bar">
-<router-link to="/" class="active">Home</router-link> 
-</li>
-<li style="display:inline">
-<router-link to="/about">About</router-link> 
-</li>
-<li style="display:inline">
-<router-link to="/resume">Resume</router-link> 
-</li>
-<li style="display:inline">
-<router-link to="/projects">Projects</router-link>
-</li>
-<li style="display:inline">
-<router-link to="/testimonials">Testimonials</router-link> 
-</li> 
-<li style="display:inline">
-<router-link to="/contact">Contact</router-link>  
-</li>
-</ul> 
-</nav>
+     <div>
+            <nav class="navbar navbar-expand-lg sticky">
+                <div class="container-fluid">
+                    <div class="logo">
+                        <img src="https://i.postimg.cc/HkZM7fBz/logo2.png"/>
+                    </div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                     <li style="display:inline" id="bar">
+                        <router-link to="/" class="active">Home</router-link> 
+                     </li>
+                     <li style="display:inline">
+                        <router-link to="/about">About</router-link> 
+                     </li>
+                     <li style="display:inline">
+                        <router-link to="/resume">Resume</router-link> 
+                     </li>
+                     <li style="display:inline">
+                        <router-link to="/projects">Projects</router-link>
+                     </li>
+                     <li style="display:inline">
+                        <router-link to="/testimonials">Testimonials</router-link> 
+                     </li> 
+                     <li style="display:inline">
+                        <router-link to="/contact">Contact</router-link>  
+                     </li>
+                    </ul>
+                </div>
+                </div>
+            </nav>
+        </div>
 </template>
 
 
